40bf9d9ebda825f2d5df73228a8f6e7aeb4cc3da
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-01-21 11:47:16 -0500

    Change day 3 part 1 solution

--- src/day03/day03.py
+++ src/day03/day03.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
-from typing import Dict, Tuple, List, Optional, Type
+from typing import Dict, Tuple, List, Optional, Type, Set
 
-INVALID_CHARS = [".", "\n"]
+INVALID_CHARS = ["."]
 
 Coordinates = namedtuple("Coordinates", ["x", "y"])
 
@@ -11,90 +11,58 @@ def parsed_input() -> List[List[str]]:
         return [list(line.strip()) for line in data.readlines()]
 
 
-def to_coordinate_dict(matrix: List[List[str]]) -> Dict[Coordinates, str]:
+def is_symbol(cell: str):
+    return not cell.isdigit() and cell not in INVALID_CHARS
+
+
+def to_coordinates(matrix: List[List[str]]) -> Set[Coordinates]:
     return {
-        Coordinates(x, y): cell
+        Coordinates(x, y)
         for x, line in enumerate(matrix)
-        for y, cell in enumerate(line)
+        for y in range(len(line))
     }
 
 
-def part_one(input_):
-    coordinate_dict = to_coordinate_dict(input_)
+def coordinate_has_adjacent_symbols(matrix: List[List[str]], coordinates_list: Set[Coordinates], coordinates: Coordinates):
+    return any(
+        is_symbol(cell)
+        for cell in get_adjacent_cells(matrix, coordinates_list, coordinates)
+    )
 
-    def get_full_number(
-        acc: Dict[Coordinates, str], coordinates: Coordinates
-    ) -> Dict[Coordinates, str]:
-        if (
-            coordinate_dict[coordinates].isdigit()
-            and (coordinates.x, coordinates.y + 1) in coordinate_dict.keys()
-        ):
-            acc_ = {
-                **acc,
-                **{coordinates: coordinate_dict[(coordinates.x, coordinates.y)]},
-            }
-            coordinates_ = Coordinates(coordinates.x, coordinates.y + 1)
-            return get_full_number(acc_, coordinates_)
-        elif coordinate_dict[coordinates].isdigit():
-            return {**acc, **{coordinates: coordinate_dict[coordinates]}}
-        else:
-            return acc
 
-    def adjacent_cells(coordinates: Coordinates) -> Dict[Coordinates, str]:
-        x, y = coordinates
-        adjacents_x = x - 1, x, x + 1
-        adjacents_y = y - 1, y, y + 1
-        adjacent_coordinates = [
-            (i, j)
-            for i in adjacents_x
-            for j in adjacents_y
-            if (i, j) in coordinate_dict.keys()
-        ]
-        return {
-            coordinates: coordinate_dict[coordinates]
-            for coordinates in adjacent_coordinates
-        }
+def get_adjacent_cells(matrix: List[List[str]], coordinates_list: Set[Coordinates], coordinates: Coordinates) -> List[str]:
+    x, y = coordinates
+    adjacents_x = x - 1, x, x + 1
+    adjacents_y = y - 1, y, y + 1
 
-    def has_adjacent_symbols(number: Dict[Coordinates, str]):
-        def coordinate_has_adjacent_symbols(coordinates: Coordinates):
-            return any(
-                adjacent_cell_coordinates in symbols.keys()
-                for adjacent_cell_coordinates in adjacent_cells(coordinates).keys()
-            )
-
-        return any(
-            coordinate_has_adjacent_symbols(coordinates)
-            for coordinates in number.keys()
-        )
-
-    def value_to_left(coordinates: Coordinates) -> Optional[str]:
-        return (
-            coordinate_dict[(coordinates.x, coordinates.y - 1)]
-            if (coordinates.x, coordinates.y - 1) in coordinate_dict.keys()
-            else None
-        )
-
-    # find all numbers
-    first_digits = {
-        Coordinates(*k): v
-        for k, v in coordinate_dict.items()
-        if v.isdigit() and (value_to_left(k) is None or not value_to_left(k).isdigit())
-    }
-    numbers = [
-        get_full_number({coordinates: value}, coordinates)
-        for coordinates, value in first_digits.items()
+    return [
+        matrix[i][j]
+        for i in adjacents_x
+        for j in adjacents_y
+        if (i, j) in coordinates_list
     ]
-    # find all symbols
-    symbols = {
-        Coordinates(*k): v
-        for k, v in coordinate_dict.items()
-        if v.isdigit() is False and v not in INVALID_CHARS
-    }
-    # filter numbers adjacent to symbols
-    valid_numbers = [number for number in numbers if has_adjacent_symbols(number)]
-    valid_numbers_int = [int("".join(n.values())) for n in valid_numbers]
-    # get the sum
-    return sum(int("".join(n.values())) for n in valid_numbers)
+
+
+def part_one(matrix: List[List[str]]):
+    coordinates = to_coordinates(matrix)
+    res = 0
+    current_number = ""
+    current_number_is_valid = False
+    for i, line in enumerate(matrix):
+        for y, cell in enumerate(line):
+            if cell.isdigit() and coordinate_has_adjacent_symbols(matrix, coordinates, (i, y)):
+                current_number_is_valid = True
+
+            if cell.isdigit():
+                current_number += cell
+            elif len(current_number) > 0 and current_number_is_valid:
+                res += int(current_number)
+                current_number = ""
+                current_number_is_valid = False
+            else:
+                current_number = ""
+                current_number_is_valid = False
+    return res
 
 
 def part_two(input_):
