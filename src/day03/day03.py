86160923a4ad6e885e69eca4f5df8fd541b7143d
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-01-22 20:04:41 -0500

    Day 3 part 2 ✌️

--- src/day03/day03.py
+++ src/day03/day03.py
@@ -1,18 +1,13 @@
 from collections import namedtuple
-from typing import Dict, Tuple, List, Optional, Type, Set
-
-INVALID_CHARS = ["."]
+from math import prod
+from typing import Dict, List, Set
 
 Coordinates = namedtuple("Coordinates", ["x", "y"])
 
 
-def parsed_input() -> List[List[str]]:
+def parsed_input() -> List[str]:
     with open("input.dat", "r") as data:
-        return [list(line.strip()) for line in data.readlines()]
-
-
-def is_symbol(cell: str):
-    return not cell.isdigit() and cell not in INVALID_CHARS
+        return [line.strip() for line in data.readlines()]
 
 
 def to_coordinates_dict(matrix: List[str]) -> Dict[Coordinates, str]:
@@ -23,20 +18,39 @@ def to_coordinates_dict(matrix: List[str]) -> Dict[Coordinates, str]:
     }
 
 
-def coordinate_has_adjacent_symbols(coordinates_dict: Dict[Coordinates, str], coordinates: Coordinates):
+def coordinate_has_adjacent_symbols(
+    coordinates_dict: Dict[Coordinates, str], coordinates: Coordinates
+):
+    def is_symbol(cell: str):
+        return not cell.isdigit() and cell != "."
+
     return any(
         is_symbol(cell)
         for cell in get_adjacent_cells(coordinates_dict, coordinates).values()
     )
 
 
-def get_adjacent_cells(coordinates_dict: Dict[Coordinates, str], coordinates: Coordinates) -> Dict[Coordinates, str]:
+def get_adjacent_stars_coordinates(
+    coordinates_dict: Dict[Coordinates, str], coordinates: Coordinates
+) -> Set[Coordinates]:
+    return {
+        coordinates
+        for coordinates, cell in get_adjacent_cells(
+            coordinates_dict, coordinates
+        ).items()
+        if cell == "*"
+    }
+
+
+def get_adjacent_cells(
+    coordinates_dict: Dict[Coordinates, str], coordinates: Coordinates
+) -> Dict[Coordinates, str]:
     x, y = coordinates
     adjacents_x = x - 1, x, x + 1
     adjacents_y = y - 1, y, y + 1
 
     return {
-        (i,j): coordinates_dict[(i,j)]
+        (i, j): coordinates_dict[(i, j)]
         for i in adjacents_x
         for j in adjacents_y
         if (i, j) in coordinates_dict.keys()
@@ -50,13 +64,17 @@ def part_one(matrix: List[str]):
     current_number_is_valid = False
     for i, line in enumerate(matrix):
         for j, cell in enumerate(line):
-            if cell.isdigit() and coordinate_has_adjacent_symbols(coordinates_dict, (i, j)):
+            if cell.isdigit() and coordinate_has_adjacent_symbols(
+                coordinates_dict, (i, j)
+            ):
                 current_number_is_valid = True
+                current_number += cell
 
-            if cell.isdigit():
+            elif cell.isdigit():
                 current_number += cell
             elif len(current_number) > 0 and current_number_is_valid:
                 res += int(current_number)
+
                 current_number = ""
                 current_number_is_valid = False
             else:
@@ -65,10 +83,47 @@ def part_one(matrix: List[str]):
     return res
 
 
-def part_two(input_):
-    pass
+def part_two(matrix: List[str]):
+    coordinates_dict = to_coordinates_dict(matrix)
+    numbers_adjacents_to_star_symbols: Dict[Coordinates, List[int]] = {
+        k: [] for k, v in coordinates_dict.items() if v == "*"
+    }
+
+    def track_numbers_adjacents_to_star_symbols(adjacent_star_coordinates_, current_number_):
+        for coordinates in adjacent_star_coordinates_:
+            numbers_adjacents_to_star_symbols[coordinates].append(
+                int(current_number_)
+            )
+
+    current_number = ""
+    adjacent_star_coordinates = set()
+    for i, line in enumerate(matrix):
+        for j, cell in enumerate(line):
+            adjacent_stars = get_adjacent_stars_coordinates(coordinates_dict, (i, j))
+            if cell.isdigit() and len(adjacent_stars) > 0:
+                adjacent_star_coordinates = adjacent_star_coordinates.union(
+                    adjacent_stars
+                )
+                current_number += cell
+            elif cell.isdigit():
+                current_number += cell
+            elif len(current_number) > 0 and len(adjacent_star_coordinates) > 0:
+                track_numbers_adjacents_to_star_symbols(adjacent_star_coordinates, current_number)
+
+                current_number = ""
+                adjacent_star_coordinates = set()
+            else:
+                current_number = ""
+                adjacent_star_coordinates = set()
+
+    gear_ratios = (
+        prod(numbers)
+        for numbers in numbers_adjacents_to_star_symbols.values()
+        if len(numbers) == 2
+    )
+
+    return sum(gear_ratios)
 
 
 if __name__ == "__main__":
     print(part_one(parsed_input()), part_two(parsed_input()))
-    assert part_one(parsed_input()) == 535235
