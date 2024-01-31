a50b1e9147a3b505c12b90b47134e2c998dba5c2
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-01-30 21:11:57 -0500

    Day 5 part 2 ✌️

--- src/day05/day05.py
+++ src/day05/day05.py
@@ -1,5 +1,7 @@
 import re
-from typing import Dict, Iterable, List, Tuple, TypedDict
+from typing import Dict, Iterable, List
+
+Section = Dict[range, range]
 
 
 def parsed_input():
@@ -7,7 +9,46 @@ def parsed_input():
         return [line.strip() for line in data.readlines()]
 
 
-Section = Dict[int, int]
+def part_of(candidate: range, ranges: Iterable[range]):
+    return next(
+        r for r in ranges if r.start <= candidate.start and r.stop >= candidate.stop
+    )
+
+
+def distincts(seed: range, keys: Iterable[range]) -> List[range]:
+    candidates = [seed]
+    result = []
+    while len(candidates) > 0:
+        candidate = candidates.pop()
+        matched = False
+        for key in keys:
+            left_overflow = candidate.start < key.start
+            right_overflow = candidate.stop > key.stop
+
+            if len(overlap(candidate, [key])) > 0:
+                matched = True
+                match left_overflow, right_overflow:
+                    case True, True:
+                        candidates.append(range(candidate.start, key.start))
+                        candidates.append(range(key.stop, candidate.stop))
+                    case True, False:
+                        candidates.append(range(candidate.start, key.start))
+                    case False, True:
+                        candidates.append(range(key.stop, candidate.stop))
+                    case False, False:
+                        pass
+        if matched is False:
+            result.append(candidate)
+            break
+        matched = False
+
+    return result
+
+
+def overlap(seed: range, keys: Iterable[range]):
+    candidates = [range(max(seed[0], key[0]), min(seed[-1], key[-1]) + 1) for key in keys]
+    return [candidate for candidate in candidates if len(candidate) > 0]
+
 
 
 def to_section(raw: List[str]) -> Section:
@@ -18,10 +59,9 @@ def to_section(raw: List[str]) -> Section:
         destination, source, range_ = [
             int(match.group()) for match in re.finditer(r"\d+", line)
         ]
-        for s, d in zip(
-            range(source, source + range_), range(destination, destination + range_)
-        ):
-            section[s] = d
+        section[range(source, source + range_)] = range(
+            destination, destination + range_
+        )
 
     return section
 
@@ -33,24 +73,46 @@ def to_sections(input_: str) -> List[Section]:
     return [to_section(raw_section) for raw_section in raw_sections]
 
 
-def traverse(sections_: List[Section], source: int):
+def traverse(sections_: List[Section], seeds: List[range]) -> List[range]:
+    def get_destination(section_: Section, seeds_: List[range]):
+        overlaps = []
+        distincts_ = []
+        for seed in seeds_:
+            seed_overlaps = overlap(seed, section_.keys())
+            overlaps += seed_overlaps
+            distincts_ += distincts(seed, section.keys())
+            if not len(seed_overlaps):
+                distincts_ += [seed]
+        overlaps_keys = [part_of(o, section_.keys()) for o in overlaps]
+
+        return [
+            range(
+                section_[key][o.start - key.start],
+                section_[key][o.stop - key.start - 1] + 1,
+            )
+            for o, key in zip(overlaps, overlaps_keys)
+        ] + distincts_
+
     if not sections_:
-        return source
+        return seeds
     section = sections_[0]
-    destination = section.get(source, source)
+    destination = get_destination(section, seeds)
     return traverse(sections_[1:], destination)
 
 
 def part_one(lines: List[str]):
     def to_seeds(line: str):
-        return [int(match.group()) for match in re.finditer(r"\d+", line)]
+        return [
+            range(int(match.group()), int(match.group()) + 1)
+            for match in re.finditer(r"\d+", line)
+        ]
 
     seeds = to_seeds(lines[0])
     sections = to_sections("\n".join(lines[1:]))
 
-    locations = [traverse(sections, seed) for seed in seeds]
+    locations = traverse(sections, seeds)
 
-    return min(locations)
+    return min(location.start for location in locations)
 
 
 def part_two(lines: List[str]):
@@ -66,11 +128,12 @@ def part_two(lines: List[str]):
     ranges = to_range_seeds(lines[0])
     sections = to_sections("\n".join(lines[1:]))
 
-    locations = [traverse(sections, seed) for r in ranges for seed in r]
+    locations = traverse(sections, ranges)
 
-    return min(locations)
+    return min(location.start for location in locations)
 
 
 if __name__ == "__main__":
-    assert part_one(parsed_input()) == 289863851
+    # assert part_one(parsed_input()) == 289863851
+    assert part_two(parsed_input()) > 36040106
     print(part_one(parsed_input()), part_two(parsed_input()))
