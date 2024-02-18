49452a060f3db71dd9c8212cbbb66e26b9c8a73c
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-02-18 09:40:59 -0500

    Day 9 part 2 ✌️

--- src/day09/day09.py
+++ src/day09/day09.py
@@ -17,10 +17,17 @@ def to_differences(numbers: List[Deque[int]]):
     return numbers
 
 
-def with_added_values(differences: List[Deque[int]]):
+def with_added_values_right(differences: List[Deque[int]]):
     while len(differences) > 1:
         last = differences.pop()
         differences[-1].append(last[-1] + differences[-1][-1])
+    return differences.pop()
+
+
+def with_added_values_left(differences: List[Deque[int]]):
+    while len(differences) > 1:
+        last = differences.pop()
+        differences[-1].appendleft(differences[-1][0] - last[0])
     return differences.pop()
 
 
@@ -28,9 +35,22 @@ def with_added_values(differences: List[Deque[int]]):
 def part_one(lines: List[str]):
     values = [deque(int(s) for s in line.split(" ")) for line in lines]
 
-    return sum(with_added_values(to_differences([value])).pop() for value in values)
+    return sum(
+        with_added_values_right(to_differences([value])).pop() for value in values
+    )
+
+
+@benchmark
+def part_two(lines: List[str]):
+    values = [deque(int(s) for s in line.split(" ")) for line in lines]
+
+    return sum(
+        with_added_values_left(to_differences([value])).popleft() for value in values
+    )
 
 
 if __name__ == "__main__":
     assert part_one(parsed_input()) == 1884768153
+
     print(part_one(parsed_input()))
+    print(part_two(parsed_input()))
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -1,16 +1,11 @@
-from src.day09.day09 import part_one
+from src.day09.day09 import part_one, part_two
 
-EXAMPLE = [
-    "0 3 6 9 12 15",
-    "1 3 6 10 15 21",
-    "10 13 16 21 30 45"
-]
+EXAMPLE = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"]
 
 
 def test_part_one():
     assert part_one(EXAMPLE) == 114
 
 
-
 def test_part_two():
-    pass
+    assert part_two(EXAMPLE) == 2
