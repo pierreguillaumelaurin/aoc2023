bd735ccd5412ac465ad7def6eb0d917a388d2f01
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-03-17 21:03:37 -0400

    Day 16 part 2 🍀🍀

--- src/day16/day16.py
+++ src/day16/day16.py
@@ -16,16 +16,18 @@ class Matrix:
         self._map = to_coordinates_dict(matrix)
 
     def get_energized_tile_count(
-            self, start: Coordinates, starting_direction: Coordinates
+        self, start: Coordinates, starting_direction: Coordinates
     ):
         def is_in_loop(
-                move: Coordinates, direction: Coordinates, moves_already_made: Set[Coordinates]
+            move: Coordinates,
+            direction: Coordinates,
+            moves_already_made: Set[Coordinates],
         ):
             print(moves_already_made)
             return (move, direction) in moves_already_made
 
         moves_already_made = set()
-        first_move = ((0, 0), (0, 1))
+        first_move = (start, starting_direction)
         moves_to_make = deque()
         moves_to_make.append(first_move)
         while moves_to_make:
@@ -46,15 +48,11 @@ class Matrix:
                 case "/":
                     next_direction = (-1 * direction[1], -1 * direction[0])
                     next_move = add_coordinates(move, next_direction)
-                    moves_to_make.append(
-                        (next_move, next_direction)
-                    )
+                    moves_to_make.append((next_move, next_direction))
                 case "\\":
                     next_direction = (direction[1], direction[0])
                     next_move = add_coordinates(move, next_direction)
-                    moves_to_make.append(
-                        (next_move, next_direction)
-                    )
+                    moves_to_make.append((next_move, next_direction))
                 case "|":
                     next_first_direction = (
                         direction[1],
@@ -63,13 +61,8 @@ class Matrix:
                     next_first_move = add_coordinates(move, next_first_direction)
                     next_second_direction = (-1 * direction[1], -1 * direction[0])
                     next_second_move = add_coordinates(move, next_second_direction)
-                    moves_to_make.append(
-                        (next_first_move, next_first_direction)
-                    )
-                    moves_to_make.append(
-                        (next_second_move,
-                         next_second_direction)
-                    )
+                    moves_to_make.append((next_first_move, next_first_direction))
+                    moves_to_make.append((next_second_move, next_second_direction))
                 case "-":
                     next_first_direction = (
                         direction[1],
@@ -78,13 +71,8 @@ class Matrix:
                     next_first_move = add_coordinates(move, next_first_direction)
                     next_second_direction = (-1 * direction[1], -1 * direction[0])
                     next_second_move = add_coordinates(move, next_second_direction)
-                    moves_to_make.append(
-                        (next_first_move, next_first_direction)
-                    )
-                    moves_to_make.append(
-                        (next_second_move,
-                         next_second_direction)
-                    )
+                    moves_to_make.append((next_first_move, next_first_direction))
+                    moves_to_make.append((next_second_move, next_second_direction))
                 case _:
                     raise ValueError(
                         f"Oops! Here is the context: next move={move}, direction={direction}"
@@ -99,5 +87,27 @@ def part_one(raw_matrix: List[str]):
     return matrix.get_energized_tile_count((0, 0), (0, 1))
 
 
+@benchmark
+def part_two(raw_matrix: List[str]):
+    matrix = Matrix(raw_matrix)
+    max_left_side = max(
+        matrix.get_energized_tile_count((i, 0), (0, 1)) for i in range(len(raw_matrix))
+    )
+    max_right_side = max(
+        matrix.get_energized_tile_count((i, len(raw_matrix[0]) - 1), (0, -1))
+        for i in range(len(raw_matrix))
+    )
+    max_top_side = max(
+        matrix.get_energized_tile_count((0, i), (1, 0)) for i in range(len(raw_matrix))
+    )
+    max_bottom_side = max(
+        matrix.get_energized_tile_count((len(raw_matrix) - 1, i), (-1, 0))
+        for i in range(len(raw_matrix))
+    )
+    print(max_left_side, max_right_side, max_top_side, max_bottom_side)
+    return max(max_left_side, max_right_side, max_top_side, max_bottom_side)
+
+
 if __name__ == "__main__":
     print(part_one(parsed_input()))
+    print(part_two(parsed_input()))
