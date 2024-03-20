3d3a79d28312448ce8845dc19e7bd154ae4de7d2
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-03-19 21:19:32 -0400

    Day 17 part 2 🌋🌋

--- src/day17/day17.py
+++ src/day17/day17.py
@@ -1,7 +1,6 @@
 from heapq import heappop, heappush
 from typing import List
 
-from src.coordinates import add_coordinates, substract_coordinates
 from src.utils import benchmark
 
 
@@ -14,9 +13,11 @@ def parsed_input():
 def part_one(matrix: List[str]):
     seen = set()
     priority_queue = [(0, 0, 0, 0, 0, 0)]
-    directions = ((0, 1), (0, -1), (1, 0), (-1, 0)) # TODO use nested tuples instead
+    directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
     while priority_queue:
-        heat_loss, x, y, x_direction, y_direction, straight_moves = heappop(priority_queue)
+        heat_loss, x, y, x_direction, y_direction, straight_moves = heappop(
+            priority_queue
+        )
 
         if (x, y) == (len(matrix) - 1, len(matrix[0]) - 1):
             return heat_loss
@@ -25,24 +26,108 @@ def part_one(matrix: List[str]):
             continue
         seen.add((x, y, x_direction, y_direction, straight_moves))
 
-
-        if straight_moves < 3 and (x_direction, y_direction) != (0, 0): # TODO remove?
+        if straight_moves < 3:
             next_x = x + x_direction
             next_y = y + y_direction
             if 0 <= next_x < len(matrix) and 0 <= next_y < len(matrix[0]):
-                heappush(priority_queue, (heat_loss + int(matrix[next_x][next_y]), next_x, next_y, x_direction, y_direction, straight_moves + 1))
+                heappush(
+                    priority_queue,
+                    (
+                        heat_loss + int(matrix[next_x][next_y]),
+                        next_x,
+                        next_y,
+                        x_direction,
+                        y_direction,
+                        straight_moves + 1,
+                    ),
+                )
 
         for next_x_direction, next_y_direction in directions:
-            if (next_x_direction, next_y_direction) != (x_direction, y_direction) and (next_x_direction, next_y_direction) != (-x_direction, -y_direction):
+            if (next_x_direction, next_y_direction) != (x_direction, y_direction) and (
+                next_x_direction,
+                next_y_direction,
+            ) != (-x_direction, -y_direction):
                 next_x = x + next_x_direction
                 next_y = y + next_y_direction
                 if 0 <= next_x < len(matrix) and 0 <= next_y < len(matrix[0]):
-                    heappush(priority_queue, (heat_loss + int(matrix[next_x][next_y]), next_x, next_y, next_x_direction, next_y_direction, 1))
+                    heappush(
+                        priority_queue,
+                        (
+                            heat_loss + int(matrix[next_x][next_y]),
+                            next_x,
+                            next_y,
+                            next_x_direction,
+                            next_y_direction,
+                            1,
+                        ),
+                    )
+    return None
+
+
+@benchmark
+def part_two(matrix: List[str]):
+    seen = set()
+    priority_queue = [(0, 0, 0, 0, 0, 0)]
+    directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    while priority_queue:
+        heat_loss, x, y, x_direction, y_direction, straight_moves = heappop(
+            priority_queue
+        )
+
+        if (x, y) == (len(matrix) - 1, len(matrix[0]) - 1) and straight_moves >= 4:
+            return heat_loss
+
+        if (x, y, x_direction, y_direction, straight_moves) in seen:
+            continue
+        seen.add((x, y, x_direction, y_direction, straight_moves))
+
+        if straight_moves < 10:
+            next_x = x + x_direction
+            next_y = y + y_direction
+            if 0 <= next_x < len(matrix) and 0 <= next_y < len(matrix[0]):
+                heappush(
+                    priority_queue,
+                    (
+                        heat_loss + int(matrix[next_x][next_y]),
+                        next_x,
+                        next_y,
+                        x_direction,
+                        y_direction,
+                        straight_moves + 1,
+                    ),
+                )
+
+        if straight_moves >= 4 or (x_direction, y_direction) == (
+            0,
+            0,
+        ):  # TODO remove 0,0
+            for next_x_direction, next_y_direction in directions:
+                if (next_x_direction, next_y_direction) != (
+                    x_direction,
+                    y_direction,
+                ) and (next_x_direction, next_y_direction) != (
+                    -x_direction,
+                    -y_direction,
+                ):
+                    next_x = x + next_x_direction
+                    next_y = y + next_y_direction
+                    if 0 <= next_x < len(matrix) and 0 <= next_y < len(matrix[0]):
+                        heappush(
+                            priority_queue,
+                            (
+                                heat_loss + int(matrix[next_x][next_y]),
+                                next_x,
+                                next_y,
+                                next_x_direction,
+                                next_y_direction,
+                                1,
+                            ),
+                        )
     return None
 
 
 if __name__ == "__main__":
+    assert part_one(parsed_input()) == 859
     print(part_one(parsed_input()))
-
-
-
+    assert part_two(parsed_input()) == 1027
+    print(part_two(parsed_input()))
