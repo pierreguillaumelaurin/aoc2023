6c85e95e38272bad08fd14b1a9dea5ddc689f991
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2024-03-28 18:28:54 -0400

    Day 22 part 1 ⏳

---
+++ src/day22/day22.py
@@ -0,0 +1,71 @@
+import re
+from typing import Dict, Iterable, List, Tuple
+
+from src.utils import benchmark
+
+
+def parsed_input():
+    with open("./input.dat", "r") as data:
+        return [
+            list(map(int, re.split(r"[,~]", line.strip()))) for line in data.readlines()
+        ]
+
+
+Brick = Tuple[Tuple[int, int, int], Tuple[int, int, int]]
+# TODO change to has overlap
+
+
+def get_overlap(block: Brick, blocks: Iterable[Brick]):
+    candidates: Dict[Brick, Tuple[range, range]] = {
+        other_block: (
+            range(
+                max(block[0][0], other_block[0][0]),
+                min(block[-1][0], other_block[-1][0]) + 1,
+            ),
+            range(
+                max(block[0][1], other_block[0][1]),
+                min(block[-1][1], other_block[-1][1]) + 1,
+            ),
+        )
+        for other_block in blocks
+    }
+    return [
+        key
+        for key, (x_overlap, y_overlap) in candidates.items()
+        if len(x_overlap) > 0 and len(y_overlap) > 0
+    ]
+
+
+def has_overlap_above(block: Brick, blocks: List[Brick]):
+    return len(get_overlap(block, blocks[blocks.index(block) + 1 :])) > 0
+
+
+def get_dependencies(block: Brick, matrix: List[Brick]):
+    deps = get_overlap(block, matrix[: matrix.index(block)])
+    return [dep for dep in deps if not has_overlap_above(dep, deps)]
+
+
+def is_removable(block: Brick, matrix: List[Brick]):
+    blocks_above = get_overlap(block, matrix[matrix.index(block) + 1: ])
+    return (
+        not has_overlap_above(block, matrix) or not any(
+        block in (dependencies := get_dependencies(candidate, matrix))
+        and len(dependencies) == 1
+        for candidate in blocks_above
+        )
+    )
+
+
+@benchmark
+def part_one(coordinates: List[List[int]]):
+    assert all(coordinate[2] <= coordinate[5] for coordinate in coordinates)
+
+    blocks: List[Brick] = [
+        ((x1, y1, z1), (x2, y2, z2)) for x1, y1, z1, x2, y2, z2 in coordinates
+    ]
+    sorted_blocks = sorted(blocks, key=lambda x: x[0][2])
+    return sum(is_removable(block, blocks) for block in sorted_blocks)
+
+
+if __name__ == "__main__":
+    print(part_one(parsed_input()))
---
+++ tests/test_day_22.py
@@ -0,0 +1,48 @@
+from src.day22.day22 import get_overlap, has_overlap_above, part_one
+
+RAW_EXAMPLE = [
+    (1, 0, 1, 1, 2, 1),
+    (0, 0, 2, 2, 0, 2),
+    (0, 2, 3, 2, 2, 3),
+    (0, 0, 4, 0, 2, 4),
+    (2, 0, 5, 2, 2, 5),
+    (0, 1, 6, 2, 1, 6),
+    (1, 1, 8, 1, 1, 9),
+]
+
+PARSED_EXAMPLE = [
+    ((1, 0, 1), (1, 2, 1)),
+    ((0, 0, 2), (2, 0, 2)),
+    ((0, 2, 3), (2, 2, 3)),
+    ((0, 0, 4), (0, 2, 4)),
+    ((2, 0, 5), (2, 2, 5)),
+    ((0, 1, 6), (2, 1, 6)),
+    ((1, 1, 8), (1, 1, 9)),
+]
+
+
+def test_has_overlap_when_overlap_exists():
+    assert get_overlap(PARSED_EXAMPLE[0], PARSED_EXAMPLE) == [
+        ((1, 0, 1), (1, 2, 1)),
+        ((0, 0, 2), (2, 0, 2)),
+        ((0, 2, 3), (2, 2, 3)),
+        ((0, 1, 6), (2, 1, 6)),
+        ((1, 1, 8), (1, 1, 9)),
+    ]
+
+
+def test_has_overlap_when_overlap_does_not_exist():
+    a_non_overlaping_block = ((2, 0, 5), (2, 2, 5))
+    assert get_overlap(PARSED_EXAMPLE[-1], [a_non_overlaping_block]) == []
+
+
+def test_has_overlap_above_when_overlap_exists():
+    assert has_overlap_above(PARSED_EXAMPLE[1], PARSED_EXAMPLE) is True
+
+
+def test_has_overlap_above_when_overlap_does_not_exist():
+    assert has_overlap_above(PARSED_EXAMPLE[-1], PARSED_EXAMPLE) is False
+
+
+def test():
+    assert part_one(RAW_EXAMPLE) == 5
